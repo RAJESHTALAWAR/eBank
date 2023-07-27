408da4c9678190f2a33a1985e41255eb199457e4
RAJESHTALAWAR <trajesh47277@gmail.com>
2023-07-27 02:09:03 +0000

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {Route, Switch} from 'react-router-dom'
+
+import SkillCourseDetailedView from './components/SkillCourseDetailedView'
+
+import Courses from './components/Courses'
+
+import NotFound from './components/NotFound'
+
+import Header from './components/Header'
+import './App.css'
+
+const App = () => (
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Courses} />
+      <Route exact path="/courses/:id" component={SkillCourseDetailedView} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+)
+
+export default App
---
+++ src/components/SkillItems/index.js
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom'
+import {Component} from 'react'
+
+class SkillItems extends Component {
+  render() {
+    const {EachSkill} = this.props
+    const {logoUrl, name, id} = EachSkill
+    console.log(logoUrl)
+    return (
+      <li>
+        <Link to={`/courses/${id}`} className="detailed-item">
+          <li className="skill-div">
+            <img src={logoUrl} className="logo-image" alt={name} />
+            <p className="name">{name}</p>
+          </li>
+        </Link>
+      </li>
+    )
+  }
+}
+
+export default SkillItems
